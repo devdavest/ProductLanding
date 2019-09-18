6db38b540bc0cf2e6e767ec8633552c3d0f0338f
David Stankovic <devdave.st@gmail.com>
2019-09-18 15:04:30 +0200

    final commit

---
+++ dist/js/main.js
@@ -0,0 +1,55 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '20px'
+});
+sr.reveal('.showcase-left', {
+	duration: 2000,
+	origin: 'top',
+	distance: '300px'
+});
+sr.reveal('.showcase-right', {
+	duration: 2000,
+	origin: 'right',
+	distance: '300px'
+});
+sr.reveal('.showcase-btn', {
+	duration: 2000,
+	delay: 2000,
+	origin: 'bottom',
+	distance: '50px'
+});
+sr.reveal('#testimonial div', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '50px'
+});
+sr.reveal('.info-left', {
+	duration: 2000,
+	origin: 'left',
+	distance: '300px',
+	viewFactor: 0.2
+});
+sr.reveal('.info-right', {
+	duration: 2000,
+	origin: 'right',
+	distance: '300px',
+	viewFactor: 0.2
+});
+
+$(function() {
+      // Smooth Scrolling
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
